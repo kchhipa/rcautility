23d14fe07922cb0be8041db8cdb552b902107138
Shukla,Gaurav <gaurav.shukla@impetus.co.in>
2016-03-29 18:45:57 +0530

    Updated week order

--- src/main/java/com/rca/workbookutility/LoggedDefectsVsOpen.java
+++ src/main/java/com/rca/workbookutility/LoggedDefectsVsOpen.java
@@ -1,10 +1,16 @@
 package com.rca.workbookutility;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.NavigableMap;
+import java.util.NavigableSet;
+import java.util.TreeMap;
 
 import org.apache.log4j.Logger;
 
@@ -53,9 +59,8 @@ public class LoggedDefectsVsOpen {
 		int sizeWeek = allWeeks.size();
 		String status="";
 		
-		Map<String, Map<String, Integer>> defVsOpenList = new HashMap<String, Map<String, Integer>>();
+		Map<String, Map<String, Integer>> defVsOpenList = new LinkedHashMap<String, Map<String, Integer>>();
 		
-		List<LoggedDefectsVsOpen> lst = null;
 		
 		ReportUtility ru = new ReportUtility();
 		
@@ -64,7 +69,6 @@ public class LoggedDefectsVsOpen {
 			String week = allWeeks.get(i),week1="";
 			week = ru.removeYearFromWeek(week);
 			LOG.info("week =============== " + week);
-			lst = new ArrayList<LoggedDefectsVsOpen>();
 			LoggedDefectsVsOpen loggedDefOpen = new LoggedDefectsVsOpen();
 			Map<String, Integer> map = new HashMap<String, Integer>();
 		for(int j=0 ; j < allWeeksrcaCounts.size() ; j++){
@@ -85,8 +89,6 @@ public class LoggedDefectsVsOpen {
 					LOG.info("Status is " + status);
 					loggedDefOpen.setCloseDefectType1(status);
 					loggedDefOpen.setCount(ru.weeklyCloseTicketForAllIssuesInClose(rcaCount));
-					LOG.info("size of list and week is " + lst.size() + " key " + key);
-					 lst.add(loggedDefOpen);
 					 map.put(status, ru.weeklyCloseTicketForAllIssuesInClose(rcaCount));
 				}
 				
@@ -95,9 +97,7 @@ public class LoggedDefectsVsOpen {
 					loggedDefOpen.setOpenDefectType(status);
 					loggedDefOpen.setCount(ru.weeklyTotalOpenCountIssues(rcaCount));
 					LOG.info("Status is " + status);
-					 LOG.info("size of list and week is " + lst.size() + " key " + key);
-					 lst.add(loggedDefOpen);
-					 map.put(status, ru.weeklyTotalOpenCountIssues(rcaCount));
+					map.put(status, ru.weeklyTotalOpenCountIssues(rcaCount));
 					 
 				}
 				key = week;
@@ -106,6 +106,7 @@ public class LoggedDefectsVsOpen {
 			}
 		 if(!key.equals("") && key != null){
 			 defVsOpenList.put(key, map); 
+			 
 		 }
 		  }
 //		      defVsOpenList.put(key, lst);
